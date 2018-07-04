09f63aed48d1957b02c53b9890a52f4f57b9ba46
youhh <youhh@diandainfo.com>
2018-07-04 17:24:05 +0800

    2018年 7月 4日 星期三 17时23分37秒 CST

--- netty-len2/src/main/java/com/phei/netty/protocol/http/xml/server/HttpXmlServer.java
+++ netty-len2/src/main/java/com/phei/netty/protocol/http/xml/server/HttpXmlServer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013-2018 Lilinfeng.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,57 +34,51 @@ import com.phei.netty.protocol.http.xml.pojo.Order;
 
 /**
  * @author lilinfeng
- * @date 2014年2月14日
  * @version 1.0
+ * @date 2014年2月14日
  */
 public class HttpXmlServer {
-    public void run(final int port) throws Exception {
-	EventLoopGroup bossGroup = new NioEventLoopGroup();
-	EventLoopGroup workerGroup = new NioEventLoopGroup();
-	try {
-	    ServerBootstrap b = new ServerBootstrap();
-	    b.group(bossGroup, workerGroup)
-		    .channel(NioServerSocketChannel.class)
-		    .childHandler(new ChannelInitializer<SocketChannel>() {
-			@Override
-			protected void initChannel(SocketChannel ch)
-				throws Exception {
-			    ch.pipeline().addLast("http-decoder",
-				    new HttpRequestDecoder());
-			    ch.pipeline().addLast("http-aggregator",
-				    new HttpObjectAggregator(65536));
-			    ch.pipeline()
-				    .addLast(
-					    "xml-decoder",
-					    new HttpXmlRequestDecoder(
-						    Order.class, true));
-			    ch.pipeline().addLast("http-encoder",
-				    new HttpResponseEncoder());
-			    ch.pipeline().addLast("xml-encoder",
-				    new HttpXmlResponseEncoder());
-			    ch.pipeline().addLast("xmlServerHandler",
-				    new HttpXmlServerHandler());
-			}
-		    });
-	    ChannelFuture future = b.bind(new InetSocketAddress(port)).sync();
-	    System.out.println("HTTP订购服务器启动，网址是 : " + "http://localhost:"
-		    + port);
-	    future.channel().closeFuture().sync();
-	} finally {
-	    bossGroup.shutdownGracefully();
-	    workerGroup.shutdownGracefully();
-	}
-    }
+
 
     public static void main(String[] args) throws Exception {
-	int port = 8080;
-	if (args.length > 0) {
-	    try {
-		port = Integer.parseInt(args[0]);
-	    } catch (NumberFormatException e) {
-		e.printStackTrace();
-	    }
-	}
-	new HttpXmlServer().run(port);
+        int port = 8080;
+        if (args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        new HttpXmlServer().run(port);
     }
+
+
+    public void run(final int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                            ch.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
+                            ch.pipeline().addLast( "xml-decoder", new HttpXmlRequestDecoder(Order.class, true));
+                            ch.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+                            ch.pipeline().addLast("xml-encoder", new HttpXmlResponseEncoder());
+                            ch.pipeline().addLast("xmlServerHandler", new HttpXmlServerHandler());
+                        }
+                    });
+
+            ChannelFuture future = b.bind(new InetSocketAddress(port)).sync();
+            System.out.println("HTTP订购服务器启动，网址是 : " + "http://localhost:" + port);
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
 }
