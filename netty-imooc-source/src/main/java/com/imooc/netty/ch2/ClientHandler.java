cd248bd378c2dcf6d4f4c56476fce8be8613c571
youhh <16621043230@163.com>
2019-03-08 22:12:23 +0800

    imooc netty实战源码

---
+++ netty-imooc-source/src/main/java/com/imooc/netty/ch2/ClientHandler.java
@@ -0,0 +1,46 @@
+package com.imooc.netty.ch2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class ClientHandler {
+
+    public static final int MAX_DATA_LEN = 1024;
+    private final Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void start() {
+        System.out.println("新客户端接入");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doStart();
+            }
+        }).start();
+    }
+
+    private void doStart() {
+        try {
+            InputStream inputStream = socket.getInputStream();
+            socket.setSoTimeout(100);
+            while (true) {
+                byte[] data = new byte[MAX_DATA_LEN];
+                int len;
+                while ((len = inputStream.read(data)) != -1) {
+                    String message = new String(data, 0, len);
+                    System.out.println("客户端传来消息: " + message);
+                    socket.getOutputStream().write(data);
+                }
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
